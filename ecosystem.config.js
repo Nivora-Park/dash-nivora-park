2c741344a5edad5347e8990ab829b93653d694a2
LAPTOP-1V3MMI7P\angel <gandha.ryanto@idpro.co.id>
2025-08-16 12:42:48 +0700

    update port number to 3005 in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env: {
-        PORT: 3000,
+        PORT: 3005,
         NODE_ENV: 'production'
       },
   pre_start: 'npm run build',
